f32e8f7e81f4b10eb1e761d02f765b5dcd52fc3c
kevinhtran <kevdeveloper7@gmail.com>
2021-04-09 21:11:17 -0700

    setting up file structure

---
+++ src/pages/company/history.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const History = () => {
+  return <div>this is history of our company</div>;
+};
+
+export default History;
---
+++ src/pages/testing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const testing = () => {
+  return <div>ERROR</div>;
+};
+
+export default testing;
