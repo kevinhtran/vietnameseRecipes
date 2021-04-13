a727835f84e2d777b0faf65e1e20192b7ad5eac9
kevinhtran <kevdeveloper7@gmail.com>
2021-04-13 13:52:06 -0700

    testing out css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { page, text } from '../examples/about.module.css';
 
 const About = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
         <h1>about page</h1>
+        <h1>hello world</h1>
+        <p className={text}>orem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
       </div>
     </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import { page, text } from '../examples/home.module.css';
 
 const Home = () => {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>homepage</h1>
         <h1>hello people</h1>
         <h1>hello world</h1>
+        <p className={text}>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
       </div>
     </Layout>
   );
