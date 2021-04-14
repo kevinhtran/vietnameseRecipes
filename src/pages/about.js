25e37fdc39541150ed516a35c8c3a0d5c4a1bc91
kevinhtran <kevdeveloper7@gmail.com>
2021-04-13 21:28:24 -0700

    testing styled components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { page, text } from '../examples/about.module.css';
+import styled from 'styled-components';
 
 const About = () => {
   return (
     <Layout>
-      <div className={page}>
+      <Wrapper>
         <h1>about page</h1>
         <h1>hello world</h1>
-        <p className={text}>orem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
-      </div>
+        <p className="text">Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+      </Wrapper>
     </Layout>
   );
 };
+
+const Wrapper = styled.section`
+  color: yellowgreen;
+  h1 {
+    color: purple;
+  }
+  .text {
+    text-transform: uppercase;
+  }
+`;
 
 export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import { page, text } from '../examples/home.module.css';
+import { ExampleButton } from '../examples/button';
 
 const Home = () => {
   return (
     <Layout>
       <div className={page}>
+        <ExampleButton>click me</ExampleButton>
         <h1>homepage</h1>
         <h1>hello people</h1>
         <h1>hello world</h1>
