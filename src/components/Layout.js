6c425a602a5faa36b7f863e401520e346b610ad3
kevinhtran <kevdeveloper7@gmail.com>
2021-04-14 01:59:52 -0700

    installed normalize.css and import main.css to layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+import '../assets/css/main.css';
+
 const Layout = ({ children }) => {
   return (
     <>
