8bd98d73a06b3b1a57a58a21e15a775c85c9f2f6
kevinhtran <kevdeveloper7@gmail.com>
2021-04-15 16:42:51 -0700

    fixed typos

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -2,14 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { StaticImage } from 'gatsby-plugin-image';
-
 const Images = () => {
   return (
     <Wrapper>
       <article>
-        <h4>constrained / default</h4>
+        <h4>constrained </h4>
         <StaticImage
-          src="../assets/images/recipes/recipe-1.jpeg"
+          src="../assets/images/recipe-1.jpeg"
           alt="food"
           placeholder="tracedSVG"
           layout="constrained"
@@ -20,19 +19,19 @@ const Images = () => {
       <article>
         <h4>fixed</h4>
         <StaticImage
-          src="../assets/images/recipes/recipe-1.jpeg"
+          src="../assets/images/recipe-1.jpeg"
           alt="food"
           placeholder="blurred"
           layout="fixed"
-          widht={200}
+          width={200}
           className="example-img"
           as="div"
         />
       </article>
       <article>
-        <h4>full-width</h4>
+        <h4>full width</h4>
         <StaticImage
-          src="../assets/images/recipes/recipe-1.jpeg"
+          src="../assets/images/recipe-1.jpeg"
           alt="food"
           placeholder="dominantColor"
           layout="fullWidth"
@@ -48,6 +47,7 @@ const Wrapper = styled.section`
   width: 70vw;
   margin: 0 auto;
   display: grid;
+
   text-align: center;
   gap: 2rem;
   article {
