c112cee6ce2abf0995f4e30da55e935a43f50fb1
kevinhtran <kevdeveloper7@gmail.com>
2021-04-14 22:22:55 -0700

    fixed npm issues, updated eslint, deleted gatsby bloat using gatsby clear, some testing of responsive images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import Images from '../examples/Images';
 
 const Home = () => {
   return (
     <Layout>
       <h1>home page</h1>
+      <Images />
     </Layout>
   );
 };
