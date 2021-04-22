1e153e7838c1742675db4e9bb0cdb6e6b1de365d
kevinhtran <kevdeveloper7@gmail.com>
2021-04-21 20:08:39 -0700

    using the getImage helper function for gatsby-image-plugin

--- src/examples/Gallery.js
+++ src/examples/Gallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
 
 const query = graphql`
@@ -23,13 +23,10 @@ const Gallery = () => {
     <Wrapper>
       {nodes.map((image, index) => {
         const { name } = image;
+        const pathToImage = getImage(image);
         return (
           <article key={index} className="item">
-            <GatsbyImage
-              image={image.childImageSharp.gatsbyImageData}
-              alt={name}
-              className="gallery-img"
-            />
+            <GatsbyImage image={pathToImage} alt={name} className="gallery-img" />
             <p>{name}</p>
           </article>
         );
