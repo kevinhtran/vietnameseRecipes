aaff3bb3dacef0c35ac9aad52da4b0a90cf6d6ca
kevinhtran <kevdeveloper7@gmail.com>
2021-04-23 11:54:19 -0700

    gatsby config :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   /* Your site config here */
@@ -29,8 +32,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: ``,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: `3xfh0vmm5t7q`,
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
   ],
