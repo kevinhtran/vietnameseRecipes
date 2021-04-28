11c210780951b596f591754932ee9b012b3becba
kevinhtran <kevdeveloper7@gmail.com>
2021-04-28 12:33:21 -0700

    created helper function for tags list

--- src/utils/setupTags.js
+++ src/utils/setupTags.js
@@ -1,7 +1,22 @@
-import React from 'react';
-
 const setupTags = (recipes) => {
-  return recipes();
+  const allTags = {};
+
+  recipes.forEach((recipe) => {
+    recipe.content.tags.forEach((tag) => {
+      if (allTags[tag]) {
+        allTags[tag] = allTags[tag] + 1;
+      } else {
+        allTags[tag] = 1;
+      }
+    });
+  });
+  const newTags = Object.entries(allTags).sort((a, b) => {
+    const [firstTag] = a;
+    const [secondTag] = b;
+    return firstTag.localeCompare(secondTag);
+  });
+
+  return newTags;
 };
 
 export default setupTags;
