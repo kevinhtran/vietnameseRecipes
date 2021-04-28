36e82faa01d4d065e9fd221d7dce53218648a9de
kevinhtran <kevdeveloper7@gmail.com>
2021-04-28 15:46:56 -0700

    added link to tagslist component

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import setupTags from '../utils/setupTags';
+import { Link } from 'gatsby';
 
 const TagsList = ({ recipes }) => {
   const newTags = setupTags(recipes);
   return (
-    <div>
-      <h4>this is a tagslist</h4>
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag;
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
