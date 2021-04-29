8202b66633a01c3009267f53607cd0b38d8f3bc0
kevinhtran <kevdeveloper7@gmail.com>
2021-04-29 14:27:20 -0700

    hero - pic, title, desc.; icons, and tags

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,12 +1,69 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import { graphql, Link } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { BsClockHistory } from '@react-icons/all-files/bs/BsClockHistory';
+import { BsClock } from '@react-icons/all-files/bs/BsClock';
+import { BsPeople } from '@react-icons/all-files/bs/BsPeople';
 
-const RecipeTemplate = (props) => {
-  console.log(props);
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe;
+  const pathToImage = getImage(image);
+  const { tags, instructions, ingredients, tools } = content;
   return (
-    <div>
-      <h4>Recipe Template</h4>
-    </div>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          {/* hero */}
+          <section className="recipe-hero">
+            <GatsbyImage image={pathToImage} alt={title} className="about-img" />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>serving</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  );
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the content */}
+          <section className="recipe-content"></section>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
