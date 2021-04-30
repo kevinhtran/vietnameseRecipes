508ba302110521ecb8919b5c97c02b523a16251d
kevinhtran <kevdeveloper7@gmail.com>
2021-04-29 19:13:42 -0700

    edit to folder name

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path');
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -12,4 +13,15 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `);
+  result.data.allContentfulRecipe.nodes.forEach((recipe) => {
+    recipe.content.tags.forEach((tag) => {
+      createPage({
+        path: `/${tag}`,
+        component: path.resolve(`src/templates/tag-template.js`),
+        context: {
+          tag: tag,
+        },
+      });
+    });
+  });
 };
