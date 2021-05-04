2c4dd4575f58592601b09f70d9d9652a511475ff
kevinhtran <kevdeveloper7@gmail.com>
2021-05-04 15:53:50 -0700

    added siteMetaData query and fixed htmlAttribute lang erro

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
+
 const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query);
   return (
     <Helmet
-      htmlAttributes={{ lang: en }}
+      htmlAttributes={{ lang: 'en' }}
       title={title}
       meta={[{ name: `description`, content: description }]}
     ></Helmet>
