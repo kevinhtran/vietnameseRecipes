094788ad37e080c7548b26f9685726d7c9ff2864
kevinhtran <kevdeveloper7@gmail.com>
2021-05-04 15:58:24 -0700

    changed title to include website title

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -18,7 +18,7 @@ const SEO = ({ title, description }) => {
   return (
     <Helmet
       htmlAttributes={{ lang: 'en' }}
-      title={title}
+      title={`${title} | ${site.siteMetadata.title}`}
       meta={[{ name: `description`, content: description }]}
     ></Helmet>
   );
